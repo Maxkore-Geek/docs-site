409bb884c1f0d04767ccc4e577e783fd88b425c8
Maxkore <3371585446@qq.com>
2026-03-22 18:20:46 +0800

    Update index.js

--- src/theme/DocItem/Footer/index.js
+++ src/theme/DocItem/Footer/index.js
@@ -20,14 +20,16 @@ export default function DocItemFooter() {
               target="_blank"
               rel="noopener noreferrer"
               className="theme-edit-this-page"
+              style={{ display: 'inline-flex', alignItems: 'center', gap: '0.25rem' }}
             >
               <svg
                 fill="currentColor"
-                height="20"
-                width="20"
+                height="16"
+                width="16"
                 viewBox="0 0 40 40"
                 className="iconEdit_IMw_"
                 aria-hidden="true"
+                style={{ verticalAlign: 'middle' }}
               >
                 <g>
                   <path d="m34.5 11.7l-3 3.1-6.3-6.3 3.1-3q0.5-0.5 1.2-0.5t1.1 0.5l3.9 3.9q0.5 0.4 0.5 1.1t-0.5 1.2z m-29.5 17.1l18.4-18.5 6.3 6.3-18.4 18.4h-6.3v-6.2z" />
