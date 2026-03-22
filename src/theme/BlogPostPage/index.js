a86733aa9e8199293aeb716141e260cdb535888c
Maxkore <3371585446@qq.com>
2026-03-22 18:20:29 +0800

    Update index.js

--- src/theme/BlogPostPage/index.js
+++ src/theme/BlogPostPage/index.js
@@ -1,4 +1,3 @@
-import Comments from '@site/src/components/Comments';
 import React from 'react';
 import clsx from 'clsx';
 import {HtmlClassNameProvider, ThemeClassNames} from '@docusaurus/theme-common';
@@ -13,15 +12,18 @@ import BlogPostPageMetadata from '@theme/BlogPostPage/Metadata';
 import BlogPostPageStructuredData from '@theme/BlogPostPage/StructuredData';
 import TOC from '@theme/TOC';
 import ContentVisibility from '@theme/ContentVisibility';
+import Comments from '@site/src/components/Comments';
+import Link from '@docusaurus/Link';
 
 function BlogPostPageContent({sidebar, children}) {
   const {metadata, toc} = useBlogPost();
-  const {nextItem, prevItem, frontMatter} = metadata;
+  const {nextItem, prevItem, frontMatter, editUrl, date, formattedDate, authors} = metadata;
   const {
     hide_table_of_contents: hideTableOfContents,
     toc_min_heading_level: tocMinHeadingLevel,
     toc_max_heading_level: tocMaxHeadingLevel,
   } = frontMatter;
+
   return (
     <BlogLayout
       sidebar={sidebar}
@@ -38,7 +40,36 @@ function BlogPostPageContent({sidebar, children}) {
 
       <BlogPostItem>{children}</BlogPostItem>
 
-      {/* ✅ 添加评论区 */}
+      {/* 博客页脚：编辑此页面 + 发布日期 */}
+      <footer className="theme-doc-footer docusaurus-mt-lg">
+        <div className="row margin-top--sm theme-doc-footer-edit-meta-row">
+          {editUrl && (
+            <div className="col">
+              <Link
+                href={editUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="theme-edit-this-page"
+              >
+                <svg fill="currentColor" height="20" width="20" viewBox="0 0 40 40" className="iconEdit_IMw_" aria-hidden="true">
+                  <g><path d="m34.5 11.7l-3 3.1-6.3-6.3 3.1-3q0.5-0.5 1.2-0.5t1.1 0.5l3.9 3.9q0.5 0.4 0.5 1.1t-0.5 1.2z m-29.5 17.1l18.4-18.5 6.3 6.3-18.4 18.4h-6.3v-6.2z" /></g>
+                </svg>
+                编辑此页面
+              </Link>
+            </div>
+          )}
+          <div className="col lastUpdated_OHCJ">
+            <span className="theme-last-updated">
+              发布于 <b>{formattedDate}</b>
+              {authors && authors.length > 0 && (
+                <> 由 <b>{authors.map(a => a.name).join(', ')}</b></>
+              )}
+            </span>
+          </div>
+        </div>
+      </footer>
+
+      {/* 评论区 */}
       <Comments mapping="pathname" />
 
       {(nextItem || prevItem) && (
